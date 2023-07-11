c9d04c656093156e70acd9e76d7da5ba03f82088
judy-chelangat <judysigilai75@gmail.com>
2023-07-11 13:35:34 +0300

    decremented the score when the user clicks the wrong number

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // console.log(document.querySelector('.message').textContent)
 // console.log(document.querySelector('message').textContent='yes')
 const secretNumber = Math.trunc(Math.random()*20)+1;
+let score =20;
 document.querySelector(".number").textContent=secretNumber
 function eventClick(){
     const guess =Number(document.querySelector('.guess').value)
@@ -14,9 +15,13 @@ function eventClick(){
     }
     else if(guess>secretNumber){
         document.querySelector('.message').textContent='too high'
+        score--
+        document.querySelector('.score').textContent=score
     }
     else if (guess<secretNumber){
         document.querySelector('.message').textContent='too low'
+        score--
+        document.querySelector('.score').textContent=score
     }
 }
 document.querySelector('.check').addEventListener('click',eventClick)
