2909eefd3d5f0cac0136ae5b1ecc13bfe6f1f996
judy-chelangat <judysigilai75@gmail.com>
2023-07-11 13:49:04 +0300

    added comments

--- index.js
+++ index.js
@@ -7,21 +7,38 @@ document.querySelector(".number").textContent=secretNumber
 function eventClick(){
     const guess =Number(document.querySelector('.guess').value)
     console.log(typeof guess)
+    // when there is no input
     if(!guess){
         document.querySelector('.message').textContent='No number!'
     }
+    // when player wins
     else if(guess === secretNumber){
       document.querySelector('.message').textContent='correct number'
     }
+    // when guess is too high
     else if(guess>secretNumber){
+        if(score>1){
         document.querySelector('.message').textContent='too high'
         score--
         document.querySelector('.score').textContent=score
+        }
+        else{
+            document.querySelector('.message').textContent='You lost the game'
+            document.querySelector('.score').textContent=0;
+        }
     }
+    // when guess is too low
     else if (guess<secretNumber){
-        document.querySelector('.message').textContent='too low'
-        score--
-        document.querySelector('.score').textContent=score
+        if(score>1){
+            document.querySelector('.message').textContent='too low'
+            score--
+            document.querySelector('.score').textContent=score
+            }
+            else{
+                document.querySelector('.message').textContent='You lost the game'
+                document.querySelector('.score').textContent=0;
+            }
+       
     }
 }
 document.querySelector('.check').addEventListener('click',eventClick)
